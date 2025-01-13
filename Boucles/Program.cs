6ac3806c114c05fc707897e6b8c722b3763deab0
YvesColletIdec <yves.collet.idec@gmail.com>
2025-01-13 20:23:31 +0100

    ajout projet

---
+++ Boucles/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Boucles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////tant que
+            ////imprimer les nombres 1 à 10
+            //int i = 1;
+            //while (i < 11)
+            //{
+            //    Console.WriteLine($"{++i}");
+            //    if (i == 5)
+            //    {
+            //        //sortir de la boucle
+            //        //break;
+            //        //passer à l'itération suivante
+            //        continue;
+            //    }
+            //    Console.WriteLine($"{i}");
+
+
+            //}
+
+            ////do while
+            //double num1 = 3;
+            //double num2 = 8;
+            //Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+            //do
+            //{
+            //    num1++;
+            //} while (num1 != num2);
+            //Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+            //Console.WriteLine($"num1 est égal à {num1}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ madeuxiemeappliconsole/Toto.cs
@@ -0,0 +1,64 @@
+﻿namespace monespace
+{
+    internal class Toto
+    {
+        static void Main(string[] args)
+        {
+            //ceci est une variable
+            //division <- 0 : entier
+            int division = 0;
+            /*récupération
+             de
+             la
+             valeur
+             à 
+             l'écran
+             */
+            //demander 2 mots à l'utilisateur et afficher ces 2 mots
+            string mot1, mot2;
+            Console.WriteLine("Entrez deux mots");
+            mot1 = Console.ReadLine();
+            mot2 = Console.ReadLine();
+            //Console.WriteLine("Vous avez entré les mots suivants : " + mot1 + " et " + mot2);
+            Console.WriteLine($"Vous avez entré les mots suivants : {mot1} et {mot2}");
+            //tester si les 2 mots sont identiques
+            mot1 = null;
+            if (mot1.Length <= 3)
+            {
+                Console.WriteLine("3-");
+            }
+            else if (mot1.Length >= 4 && mot1.Length <= 10)
+            {
+                Console.WriteLine("entre 4 et 10");
+            }
+            else
+            {
+                Console.WriteLine("10+");
+            }
+            //exemple boolean
+            bool estVrai = true;
+            if (!estVrai)
+            {
+                Console.WriteLine("C'est faux");
+            }
+            else
+            {
+                Console.WriteLine("C'est vrai");
+            }
+            Console.ReadLine();
+
+            //expliquer différence & et &&
+            //expliquer différence | et ||
+            int x = 1;
+            if (x > 2 && x < 10)
+            {
+                Console.WriteLine("x");
+            }
+            x = 1;
+            if (x > 2 || x < 10)
+            {
+                Console.WriteLine("x");
+            }
+        }
+    }
+}
